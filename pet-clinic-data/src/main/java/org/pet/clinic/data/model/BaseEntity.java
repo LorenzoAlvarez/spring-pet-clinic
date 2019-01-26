b34622554487ed358c0a55d21e0ff408b805ac24
LorenzoAlvarez <djzito@gmail.com>
2019-01-26 23:10:41 +0100

    adding base Entity. Closes #10

---
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/model/BaseEntity.java
@@ -0,0 +1,18 @@
+package org.pet.clinic.data.model;
+
+import java.io.Serializable;
+
+public class BaseEntity implements Serializable {
+
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+}
