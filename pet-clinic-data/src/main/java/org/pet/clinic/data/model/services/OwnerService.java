a5b951330a6f06d1794b899fd88bc6987881db29
LorenzoAlvarez <djzito@gmail.com>
2019-01-31 08:17:25 +0100

    changing Owner Service

--- pet-clinic-data/src/main/java/org/pet/clinic/data/model/services/OwnerService.java
+++ pet-clinic-data/src/main/java/org/pet/clinic/data/model/services/OwnerService.java
@@ -1,10 +1,9 @@
 package org.pet.clinic.data.model.services;
 
-import java.util.Set;
-
 import org.pet.clinic.data.model.Owner;
 
 public interface OwnerService extends CrudService<Owner, Long>{
+	
 	Owner findByLastName(String lastName);
 	
 }
